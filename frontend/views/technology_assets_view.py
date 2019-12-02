02ca98e790937e6fbdc1872c04de8ca88be5d7bc
twwc <twwc.tech@protonmail.ch>
2019-12-01 18:15:34 -0800

    <created>; technology assets view added

---
+++ frontend/views/technology_assets_view.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from tkinter import *
+from tkinter import messagebox
+
+
+class TechnologyAssetsView:
+    def __init__(self, title, bg_color):
+        root = Tk()
+        root.title(title)
+        root.configure(bg=bg_color)
+
+        root.mainloop()
