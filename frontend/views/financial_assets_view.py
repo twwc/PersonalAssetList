a2c62ad562ebcf4b9e7b9e29d12eca9af53ea1db
twwc <twwc.tech@protonmail.ch>
2019-12-04 08:02:05 -0800

    <modified>; added structure to Financial Assets View

--- frontend/views/financial_assets_view.py
+++ frontend/views/financial_assets_view.py
@@ -5,10 +5,29 @@ from tkinter import messagebox
 
 
 class FinancialAssetsView:
-    def __init__(self, title, bg_color):
+    def __init__(self, title, bg_color, lbl_text, lbl_text_color):
         root = Tk()
         root.title(title)
         root.configure(bg=bg_color)
         root.resizable(width=False, height=False)
+
+        financial_assets_entry_lblframe = LabelFrame(root, bg=bg_color, text=lbl_text, fg=lbl_text_color)
+        financial_assets_entry_lblframe.grid(row=0, column=0, pady=5, padx=5)
+        bank_name_label = Label(financial_assets_entry_lblframe, text='Bank:', bg=bg_color, font='ELITE 10 bold')
+        bank_name_label.grid(row=0, columm=0, pady=5, padx=5)
+        bank_name_entry = Entry(financial_assets_entry_lblframe, font='bold')
+        bank_name_entry.grid(row=0, column=1, pady=5, padx=5)
+        credit_name_label = Label(financial_assets_entry_lblframe, bg=bg_color, text='Credit Company:',
+                                  font='ELITE 10 bold')
+        credit_name_label.grid(row=1, column=0, pady=5, padx=5)
+        credit_name_entry = Entry(financial_assets_entry_lblframe, font='bold')
+        credit_name_entry.grid(row=1, column=1, pady=5, padx=5)
+
+        entry_buttons_frame = Frame(root, bg=bg_color)
+        entry_buttons_frame.grid(row=1, column=0, pady=5, padx=5)
+        submit_button = Button(entry_buttons_frame, text='SUBMIT', relief='raised', font='ELITE 10 bold', width=10)
+        submit_button.grid(row=0, column=0, pady=5, padx=5)
+        cancel_button = Button(entry_buttons_frame, text='CANCEL', relief='raised', font='ELITE 10 bold', width=10)
+        cancel_button.grid(row=0, column=1, pady=5, padx=5)
 
         root.mainloop()
